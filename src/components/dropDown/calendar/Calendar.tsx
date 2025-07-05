f6f8470fb8e2a4752e8658b2afa6ed90cf17b823
Erlond92 <ramilkha153@gmail.com>
2025-07-05 18:18:27 +0300

    Fix Calendar

--- src/components/dropDown/calendar/Calendar.tsx
+++ src/components/dropDown/calendar/Calendar.tsx
@@ -6,8 +6,9 @@ import {DropDown} from "@/components/dropDown/dropDown.tsx";
 export const Calendar: React.FC = () => {
 	const [ name, setName ] = useState('01.02.2025 - 01.03.2025');
 	const listYears = [];
-	const listMonth = []
+	// const listMonth = []
 
+	setName('');
 	for (let i = 1900; i<=2030; i++) {
 		listYears.push(i);
 	}
